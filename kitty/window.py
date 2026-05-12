7f4a96724d4549212729e82d791b9fa26d3e058a
Jason Yu <jasonyu.kr@gmail.com>
2026-05-12 23:30:14 +0900

    Remove bottom extent from get_text

--- kitty/window.py
+++ kitty/window.py
@@ -412,7 +412,6 @@ def as_text(
     add_wrap_markers: bool = False,
     alternate_screen: bool = False,
     add_cursor: bool = False,
-    bottom_mode: bool = False,
 ) -> str:
     lines: list[str] = []
     add_history = add_history and not (screen.is_using_alternate_linebuf() ^ alternate_screen)
@@ -437,10 +436,7 @@ def as_text(
     if add_history:
         pht = pagerhist(screen, as_ansi, add_wrap_markers)
         h: list[str] = [pht] if pht else []
-        if bottom_mode:
-            screen.as_text_for_history_buf(h.append, as_ansi, add_wrap_markers, 500)
-        else:
-            screen.as_text_for_history_buf(h.append, as_ansi, add_wrap_markers)
+        screen.as_text_for_history_buf(h.append, as_ansi, add_wrap_markers)
         if h:
             if as_ansi:
                 h[-1] += '\x1b[m'
@@ -1888,9 +1884,8 @@ class Window:
         add_wrap_markers: bool = False,
         alternate_screen: bool = False,
         add_cursor: bool = False,
-        bottom_mode: bool = False
     ) -> str:
-        return as_text(self.screen, as_ansi, add_history, add_wrap_markers, alternate_screen, add_cursor, bottom_mode)
+        return as_text(self.screen, as_ansi, add_history, add_wrap_markers, alternate_screen, add_cursor)
 
     def cmd_output(self, which: CommandOutput = CommandOutput.last_run, as_ansi: bool = False, add_wrap_markers: bool = False) -> str:
         return cmd_output(self.screen, which, as_ansi, add_wrap_markers)
